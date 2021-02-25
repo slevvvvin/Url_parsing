3c94ad4d4b65e1cc338f5d52c1bbf1bca5031450
slevvvvin <slevvvvin@gmail.com>
2021-02-25 13:11:29 +0200

    fix MR issues

--- urls_parsing.py
+++ urls_parsing.py
@@ -8,7 +8,11 @@ from datetime import datetime
 
 
 def create_parse_args():
-    """Create command line arguments"""
+    """Create command line arguments
+
+    :return args: command line arguments for running script with different options
+    :rtype args: argparse.Namespace
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-s', '--source_file',
@@ -23,28 +27,37 @@ def create_parse_args():
     return args
 
 
-def create_timestamp_directory(directory_path):
-    """Create directory for output with timestamp"""
+def create_timestamp_directory(directory_path: str) -> str:
+    """Create directory for output with timestamp.
+
+    :param directory_path: path to the created folder
+    :type directory_path: str
+
+    :return dir_with_parsed_urls: path to the folder where timestamp directory is created
+    :rtype dir_with_parsed_urls: str
+    """
+
     dir_with_parsed_urls = f'{directory_path}/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
     os.makedirs(dir_with_parsed_urls)
     return dir_with_parsed_urls
 
 
-def parse_urls(url):
-    """Remove everything from the url except the domain and path"""
+def parse_urls(url: str) -> str:
+    """Remove everything from the url except the domain and path
+
+    :param url: unparsed url with port, hash, getParameters etc.
+    :type url: str
+
+    :return parsed_url: url that contains only domain and path
+    :rtype parsed_url: str
+    """
     parsed_url = re.sub(r'^\w+://|www\.|[^/]+$', '', url)
     return parsed_url
 
 
-def open_source_file(file):
-    """Open file with unparsed urls"""
-    file_with_urls = open(format(file))
-    return file_with_urls
-
-
 def write_parsed_urls():
     """Write parsed urls to timestamp directory"""
-    with open_source_file(create_parse_args().args.source_file) as urls:
+    with open(create_parse_args().source_file) as urls:
         list_of_valid_urls = list()
         with open(
                 os.path.join(
